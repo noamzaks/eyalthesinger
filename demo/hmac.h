0c138fb3a4be8a37b8ddf73f78a28ffc42460576
Maya <mayab6@mail.tau.ac.il>
2024-11-02 19:54:42 +0200

    removed unnecessary for upload

--- demo/hmac.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <stdint.h>
-
-void hmac_sha1(const char *text, uint32_t text_len, const char* key, uint32_t key_len, char* digest);
--- demo/pbkdf2.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <stdint.h>
-
-void pbkdf2_sha1(const char* password, int password_length, const char* salt, int salt_length, uint32_t iterations, int dkLen, char* output);
